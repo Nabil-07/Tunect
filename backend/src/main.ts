f6886bedcb5df23cc1d67f2dfbb15efa9fe65a52
Nabil-07 <Nabil-07@users.noreply.github.com>
2026-01-12 23:38:29 +0530

    node18Issueresolved

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,3 +1,9 @@
+import { webcrypto } from 'crypto';
+
+if (!(globalThis as any).crypto) {
+  (globalThis as any).crypto = webcrypto;
+}
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
